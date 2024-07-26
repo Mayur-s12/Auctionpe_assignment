566bbfe7cfa61fb2ba0d72e3914f9174f6ce91f7
mayur-sahare <mayursahare91@gmail.com>
2024-07-27 01:17:15 +0530

    Implemented login feature using jwt package. Corrected signupUser function to take unique usernames . altered table to add unique constraint to username.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,22 @@
 import pool from "../config/database.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signupUser = async (req, res) => {
   const { username, password } = req.body;
 
   try {
+    const userQuery = `
+      SELECT * FROM users 
+      WHERE username=?;
+      `;
+    const [result] = await pool.query(userQuery, [username]);
+    const user = result[0];
+
+    if (user) {
+      return res.status(401).json({ message: "Username already exists" });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const signupUserQuery = `
@@ -22,5 +34,36 @@ export const signupUser = async (req, res) => {
     console.log("Error creating user", error);
 
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const loginQuery = `
+      SELECT * FROM users 
+      WHERE username=? ;
+    `;
+
+    const [result] = await pool.query(loginQuery, [username]);
+    const user = result[0];
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid User" });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (isPasswordMatched) {
+      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY);
+
+      return res.status(200).json({ token, message: "Login Successful" });
+    } else {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (err) {
+    console.log("Error Logging In", err);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 };
