f54a9cf0c0ff339f22d22ea6fbc7377da2155444
mayur-sahare <mayursahare91@gmail.com>
2024-07-27 14:15:03 +0530

    implemented get Actions By Session api. setup controller function & its route, plus corrected log action not taking action type.

--- backend/controllers/actionsController.js
+++ backend/controllers/actionsController.js
@@ -1,12 +1,12 @@
 import pool from "../config/database.js";
 
 export const logAction = async (req, res) => {
-  const { sessionId, actionType } = req.body;
+  const { sessionId, action_type } = req.body;
 
   try {
     const [result] = await pool.query(
       "INSERT INTO actions (session_id, action_type) VALUES (?, ?);",
-      [sessionId, actionType]
+      [sessionId, action_type]
     );
 
     res.status(201).json({
@@ -16,5 +16,23 @@ export const logAction = async (req, res) => {
   } catch (err) {
     console.log("Error logging action", err);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getActionsBySession = async (req, res) => {
+  const { sessionId } = req.params;
+
+  try {
+    const getActionsQuery = `
+        SELECT * FROM actions
+        WHERE session_id=? ;
+    `;
+
+    const [result] = await pool.query(getActionsQuery, [sessionId]);
+
+    res.status(200).json(result);
+  } catch (err) {
+    console.log("Error fetching actions");
+    res.status(500).json("Internal server error", err);
   }
 };
--- backend/routes/actionRoutes.js
+++ backend/routes/actionRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { logAction } from "../controllers/actionsController.js";
+import {
+  getActionsBySession,
+  logAction,
+} from "../controllers/actionsController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/log", authMiddleware, logAction);
+router.get("/:sessionId", authMiddleware, getActionsBySession);
 
 export default router;
