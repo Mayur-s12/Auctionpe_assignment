c70c3cefabcad7f0f939c39034001910f625ab1c
mayur-sahare <mayursahare91@gmail.com>
2024-07-29 20:45:34 +0530

    ++

--- backend/index.js
+++ backend/index.js
@@ -12,20 +12,12 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(
+  cors({
+    origin:
+      "https://auctionpe-assignmentbacend-apr3ty4ea-mayur-s12s-projects.vercel.app",
+  })
+);
 
 app.use(express.json());
 
--- frontend/src/config/apis.js
+++ frontend/src/config/apis.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { token } from "../utils";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL =
+  "https://auctionpe-assignmentbacend-apr3ty4ea-mayur-s12s-projects.vercel.app";
 
 export const logAction = async (sessionId, actionType) => {
   try {
