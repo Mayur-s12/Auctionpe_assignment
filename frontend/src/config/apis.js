00a73a68b07a6ab9b8a85e2803f15d88dd702921
mayur-sahare <mayursahare91@gmail.com>
2024-07-29 22:13:40 +0530

    deployed base url set

--- frontend/src/config/apis.js
+++ frontend/src/config/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { token } from "../utils";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://auctionpe-assignment-backend.onrender.com";
 
 export const logAction = async (sessionId, actionType) => {
   try {
