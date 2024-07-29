093e917a37bbd65dcb5acf94b5a13f408f632c37
mayur-sahare <mayursahare91@gmail.com>
2024-07-29 19:38:20 +0530

    configure for deployment

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,12 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type, Authorization",
   credentials: true,
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 
 app.use("/auth", authRoutes);
